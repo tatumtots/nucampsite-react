e133869526d1f6268d6a055f8930fa0ac0af8a94
Tatum Bray <tatumrbray@gmail.com>
2021-02-08 19:33:10 -0700

    Added about page, and react router

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -10,12 +11,11 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                 <Card>
                     <CardImg top src ={campsite.image} alt={campsite.name} />
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
             </div>
-        )
+        );
     };
 
     function RenderComments({comments}){
@@ -23,9 +23,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
             return(
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    {comments.map(comments => <div key={comments.id}>
-                        <p>{comments.text}</p>
-                        <p>{comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))} </p></div>)}
+                      <RenderComments comments={comments} />
                 </div>
             );
         }
@@ -34,14 +32,24 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
     function CampsiteInfo(props) { 
         if (props.campsite){
-            return (
-                <div className="container">
-                    <div className="row" >
-                        <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.campsite.comments} />
+             return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
                     </div>
-                </div> 
-             );
+                </div>
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+        );
         }
         return <div />;
     }
